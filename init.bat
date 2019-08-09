0a68ee1a80be0a373cda60a96011eccca77d8737
Julien Caillon <julien.caillon@gmail.com>
2019-08-09 10:57:58 +0200

    up

--- init.bat
+++ init.bat
@@ -11,3 +11,7 @@ dotnet new mstest
 cd ..
 dotnet sln add %sln%\%sln%.csproj
 dotnet sln add %sln%.Test\%sln%.Test.csproj
+
+rem modify the files to take into account the project name
+rem add *.targets and modify the library .csproj
+rem generate a new nuget key and put it in appveyor.xml
